bb5585928e68b256416a2bc30f0e4e60a1eff7c8
Jason Lewis <jason-amazing-race@hotmail.com>
2015-04-12 21:38:39 -0700

    Initial implementaiton of depth first search for 1 player games.
    Currenly broken.

--- src/main/java/notabot/NotABot.java
+++ src/main/java/notabot/NotABot.java
@@ -1,10 +1,12 @@
 package notabot;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.ggp.base.player.gamer.exception.GamePreviewException;
 import org.ggp.base.player.gamer.statemachine.StateMachineGamer;
 import org.ggp.base.util.game.Game;
+import org.ggp.base.util.statemachine.MachineState;
 import org.ggp.base.util.statemachine.Move;
 import org.ggp.base.util.statemachine.StateMachine;
 import org.ggp.base.util.statemachine.cache.CachedStateMachine;
@@ -14,6 +16,11 @@ import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
 import org.ggp.base.util.statemachine.implementation.prover.ProverStateMachine;
 
 public class NotABot extends StateMachineGamer{
+	private StateMachine stateMachine;
+
+	public NotABot(){
+		stateMachine = getInitialStateMachine();
+	}
 
 	@Override
 	public StateMachine getInitialStateMachine() {
@@ -32,18 +39,56 @@ public class NotABot extends StateMachineGamer{
 	public Move stateMachineSelectMove(long timeout)
 			throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
 
-		// Get list of possible moves
-		List<Move> moves = getStateMachine().getLegalMoves(getCurrentState(), getRole());
+		return getBestMove(getCurrentState());
+	}
 
-		// print all moves
-		for (Move m:moves){
-			System.out.println(m);
+	private Move getBestMove(MachineState state) throws MoveDefinitionException, TransitionDefinitionException, GoalDefinitionException{
+		System.out.println(stateMachine);
+		System.out.println(state);
+		System.out.println(getRole());
+		List<Move> moves = stateMachine.getLegalMoves(state, getRole());
+		Move bestMove = null;
+		MachineState bestEndState = null;
+
+		for (Move move:moves){
+			System.out.println(move);
+			List<Move> currMoves = new ArrayList<Move>();
+			currMoves.add(move);
+			MachineState currEndState = getBestEndState(stateMachine.getNextState(state, currMoves));
+
+			if (bestMove==null || stateMachine.getGoal(currEndState, getRole()) > stateMachine.getGoal(bestEndState, getRole())){
+				bestMove = move;
+				bestEndState = currEndState;
+			}
+
 		}
-		System.out.println();
 
-		getStateMachine();
+		return bestMove;
+	}
 
-		return moves.get(moves.size()-1);
+	private MachineState getBestEndState(MachineState state) throws MoveDefinitionException, TransitionDefinitionException, GoalDefinitionException{
+		if (stateMachine.isTerminal(state)) return state;
+
+		List<Move> moves = stateMachine.getLegalMoves(state, getRole());
+		MachineState bestEndState = null;
+
+		for (Move move:moves){
+			List<Move> currMoves = new ArrayList<Move>();
+			currMoves.add(move);
+			MachineState currEndState = getBestEndState(stateMachine.getNextState(state, currMoves));
+
+			if (bestEndState==null || stateMachine.getGoal(currEndState, getRole()) > stateMachine.getGoal(bestEndState, getRole())){
+				bestEndState = currEndState;
+			}
+
+		}
+
+		return bestEndState;
+	}
+
+
+	private List<Move> getMoves(MachineState state) throws MoveDefinitionException{
+		return getStateMachine().getLegalMoves(state, getRole());
 	}
 
 	@Override
