26d3e4f6d9de74f94a55877e2b8bdec310794bff
Jason Lewis <jason-amazing-race@hotmail.com>
2015-04-13 20:49:44 -0700

    Incorporate other player moves

--- src/main/java/notabot/NotABot.java
+++ src/main/java/notabot/NotABot.java
@@ -1,12 +1,14 @@
 package notabot;
 
-import java.util.Stack;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.ggp.base.player.gamer.exception.GamePreviewException;
 import org.ggp.base.player.gamer.statemachine.StateMachineGamer;
 import org.ggp.base.util.game.Game;
 import org.ggp.base.util.statemachine.MachineState;
 import org.ggp.base.util.statemachine.Move;
+import org.ggp.base.util.statemachine.Role;
 import org.ggp.base.util.statemachine.StateMachine;
 import org.ggp.base.util.statemachine.cache.CachedStateMachine;
 import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
@@ -33,40 +35,72 @@ public class NotABot extends StateMachineGamer{
 	public Move stateMachineSelectMove(long timeout)
 			throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
 
-		MovePath moveState = getBestMovePath(getCurrentState());
+		System.out.println("________");
+		System.out.println(getStateMachine().getLegalMoves(getCurrentState(), getRole()));
+
+		MovePath moveState = getBestMovePath(getCurrentState(), true);
 
 		return moveState.popMove();
 	}
 
-	private MovePath getBestMovePath(MachineState state) throws MoveDefinitionException, TransitionDefinitionException, GoalDefinitionException{
+	private MovePath getBestMovePath(MachineState state, boolean isFirst) throws MoveDefinitionException, TransitionDefinitionException, GoalDefinitionException{
 		if (getStateMachine().isTerminal(state)){
 			return new MovePath(getStateMachine().getGoal(state, getRole()));
 		}
 
-		MovePath bestMoveGoal = null;
-		Move bestMove = null;
+		MovePath bestWorst = null;
 
+		// get moveset for each opponent; compute number of opponent combinations
+		List<List<Move>> oppMoves = new ArrayList<List<Move>>();
+		int numCombinations = 1;
+		for (Role role: getStateMachine().getRoles()){
+			if (!role.equals(getRole())){
+				List<Move> currMoves = getStateMachine().getLegalMoves(state, role);
+				oppMoves.add(currMoves);
+				numCombinations *= currMoves.size();
+			}
+		}
+
+		// for each of our possible moves
 		for (Move move: getStateMachine().getLegalMoves(state, getRole())){
-			Stack<Move> currRoleMoves = new Stack<Move>();
-			currRoleMoves.add(move);// TODO add moves for all roles
 
-			MovePath moveGoal = getBestMovePath(getStateMachine().getNextState(state, currRoleMoves));
+			MovePath worstBest = null;
 
-			if (bestMoveGoal==null ||  moveGoal.getEndStateGoal() > bestMoveGoal.getEndStateGoal()){
-				bestMoveGoal = moveGoal;
-				bestMove = move;
-				if (bestMoveGoal.getEndStateGoal() == 100){
-					bestMoveGoal.pushMove(bestMove);
-					return bestMoveGoal;
+			// compute every possible combination of opponent moves
+			for (int i = 0; i < numCombinations; i++) {
+
+				List<Move> moveCombo = new ArrayList<Move>();
+				int divisor = 1;
+
+				// get each opponent's move for current combination
+				for (int opp = 0; opp < oppMoves.size(); opp++) {
+					List<Move> currOppMoves = oppMoves.get(opp);
+					moveCombo.add(currOppMoves.get(i/divisor % currOppMoves.size()));
+					divisor *= currOppMoves.size();
 				}
+
+				// Find the worst case with this combination
+				moveCombo.add(move);
+				MovePath currComboPath = getBestMovePath(getStateMachine().getNextState(state, moveCombo), false);
+
+				if (worstBest == null || worstBest.getEndStateGoal() > currComboPath.getEndStateGoal()){
+					worstBest = currComboPath;
+				}
+
 			}
 
+			if (isFirst) System.out.println(move + ": " + worstBest.getEndStateGoal());
+
+			if (bestWorst == null || bestWorst.getEndStateGoal() < worstBest.getEndStateGoal()){
+				bestWorst = worstBest;
+				bestWorst.pushMove(move);
+			}
 
 		}
 
-		bestMoveGoal.pushMove(bestMove);
+		if (isFirst) System.out.println();
 
-		return bestMoveGoal;
+		return bestWorst;
 	}
 
 	@Override
